8f3e5a8536b83d45fe430ee0a45f42417b14efad
TonySilva7 <tony.deve7oper@gmail.com>
2024-10-10 23:17:58 -0300

    feat: create entity request and respose to create expenses

---
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -0,0 +1,31 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashFlow.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ExpensesController : ControllerBase
+{
+
+    [HttpPost]
+    public IActionResult Register([FromBody] RequestRegisterExpense expense)
+    {
+        try
+        {
+            var useCase = new RegisterExpenseUseCase();
+            var res = useCase.Execute(expense);
+            return Created(string.Empty, res);
+        }
+        catch (ArgumentException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+        catch
+        {
+
+            return StatusCode(StatusCodes.Status500InternalServerError, "unknown error");
+        }
+    }
+}
---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -0,0 +1,45 @@
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+
+namespace CashFlow.Application.UseCases.Expenses.Register;
+public class RegisterExpenseUseCase
+{
+    public ResponseRegisteredExpense Execute(RequestRegisterExpense expense)
+    {
+       Validate(expense);
+
+        return new ResponseRegisteredExpense
+         {
+              Title = expense.Title
+         };
+    }
+
+    private void Validate(RequestRegisterExpense expense)
+    {
+        var titleIsInvalid = string.IsNullOrWhiteSpace(expense.Title);
+        var amountIsInvalid = expense.Amount < 0;
+        var dateIsInvalid = DateTime.Compare(expense.Date, DateTime.UtcNow) > 0;
+        var paymentTypeIsInvalid = !Enum.IsDefined(typeof(PaymentType), expense.PaymentType);
+        
+        if (titleIsInvalid)
+        {
+            throw new ArgumentException("Title is required");
+        }
+
+        if (amountIsInvalid)
+        {
+            throw new ArgumentException("Amount must be greater than zero");
+        }
+
+        if (dateIsInvalid)
+        {
+            throw new ArgumentException("Date is required");
+        }
+
+        if (paymentTypeIsInvalid)
+        {
+            throw new ArgumentException("Payment type is invalid");
+        }
+    }
+}
